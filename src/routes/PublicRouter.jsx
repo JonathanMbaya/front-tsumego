0bef82c4a6f22d0cc7ca858da2ce8091179a41cf
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-07-08 04:25:18 +0200

    formatage fichier router

--- src/routes/PublicRouter.jsx
+++ src/routes/PublicRouter.jsx
@@ -5,7 +5,7 @@ import HomePage from "../pages/HomePage";
 import ListTsumego from "../pages/ListTsumego";
 import TsumegoGame from "../pages/TsumegoGame";
 import SignupView from "../pages/client/SignupView/SignupView";
-import FormProblem from '../components/FormProblem/FormProblem';
+import FormProblem from "../components/FormProblem/FormProblem";
 
 function PublicRouter() {
    const [currentPage, setCurrentPage] = useState("");
@@ -39,44 +39,44 @@ function PublicRouter() {
                   return <TsumegoGame />;
                }}
             />
-                <Route
-                    path="/"
-                    element={<HomePage />}
-                    render={() => {
-                        setCurrentPage('home');
-                        return <HomePage />;
-                    }}
-                />
-                <Route
-                    path="/listgames"
-                    element={<ListTsumego/>}
-                    render={() => {
-                        setCurrentPage('listgames');
-                        return <ListTsumego/>;
-                    }}
-                />
+            <Route
+               path="/"
+               element={<HomePage />}
+               render={() => {
+                  setCurrentPage("home");
+                  return <HomePage />;
+               }}
+            />
+            <Route
+               path="/listgames"
+               element={<ListTsumego />}
+               render={() => {
+                  setCurrentPage("listgames");
+                  return <ListTsumego />;
+               }}
+            />
 
-                <Route
-                    path="/listgames/game"
-                    element={<TsumegoGame/>}
-                    render={() => {
-                        setCurrentPage('game');
-                        return <TsumegoGame/>;
-                    }}
-                />
+            <Route
+               path="/listgames/game"
+               element={<TsumegoGame />}
+               render={() => {
+                  setCurrentPage("game");
+                  return <TsumegoGame />;
+               }}
+            />
 
-                <Route
-                    path="/submit-problem"
-                    element={<FormProblem/>}
-                    render={() => {
-                        setCurrentPage('game');
-                        return <FormProblem/>;
-                    }}
-                />
-                <Route path="/signup" element={<SignupView />} />
-            </Routes>
-        </>
-    );
-};
+            <Route
+               path="/submit-problem"
+               element={<FormProblem />}
+               render={() => {
+                  setCurrentPage("game");
+                  return <FormProblem />;
+               }}
+            />
+            <Route path="/signup" element={<SignupView />} />
+         </Routes>
+      </>
+   );
+}
 
 export default PublicRouter;
