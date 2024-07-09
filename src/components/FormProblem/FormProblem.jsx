9d7bfd7a52df6363d149161c1c0b5260aed15184
unknown <jonathanmbaya13@gmail.com>
2024-07-09 18:07:12 +0200

    Pause dev

--- src/components/FormProblem/FormProblem.jsx
+++ src/components/FormProblem/FormProblem.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { Goban as ReactGoban }  from 'react-goban';
 import './FormProblem.css';
 
 function FormProblem() {
   const [email, setEmail] = useState('');
   const [description, setDescription] = useState('');
+  // État pour enregistrer les pierres placées
+  const [registeredStones, setRegisteredStones] = useState({});
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -23,36 +26,54 @@ function FormProblem() {
     setDescription('');
   };
 
+  // Fonction pour gérer le clic sur une intersection
+  const handleIntersectionClick = (intersection) => {
+    setRegisteredStones((prevStones) => {
+      const newStones = { ...prevStones };
+      newStones[intersection] = 'black'; // Exemple pour ajouter une pierre noire
+      return newStones;
+    }
+  );
+  }
   return (
     <div className="form-container">
       <h1>Soumettre le problème</h1>
+
+      <ReactGoban
+        size="19"
+        theme="classic"
+        stones={registeredStones}
+        nextToPlay="black"
+        onIntersectionClick={handleIntersectionClick}
+      />
+
       <form onSubmit={handleSubmit}>
         <div className='item-form'>
             
-            <label htmlFor="email">Votre Email</label>
+          <label htmlFor="email">Votre Email</label>
 
-            <input
-            type="email"
-            placeholder="Votre email"
-            className="input-email"
-            value={email}
-            onChange={handleEmailChange}
-            required
-            />
+          <input
+          type="email"
+          placeholder="Votre email"
+          className="input-email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+          />
 
         </div>
 
         <div className='item-form'>
 
-            <label htmlFor="description">Décrivez votre problème</label>
+          <label htmlFor="description">Décrivez votre problème</label>
 
-            <textarea
-            placeholder="Décrivez votre problème en quelques mots"
-            className="input-description"
-            value={description}
-            onChange={handleDescriptionChange}
-            required
-            />
+          <textarea
+          placeholder="Décrivez votre problème en quelques mots"
+          className="input-description"
+          value={description}
+          onChange={handleDescriptionChange}
+          required
+          />
 
         </div>
 
