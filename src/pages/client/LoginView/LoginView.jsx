6b36ccf9ff7f15035a1278b2e9de40bd99383e0e
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-07-10 07:20:08 +0200

    Stylisation de la page de connexion

--- src/pages/client/LoginView/LoginView.jsx
+++ src/pages/client/LoginView/LoginView.jsx
@@ -2,8 +2,9 @@ import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 import React, { useEffect, useState } from "react";
 import { login } from "../../../services/api/user";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { getUser, setLoggedUser } from "../../../services/session/session";
+import "./LoginView.css"
 
 const LoginView = () => {
     const location = useLocation()
@@ -12,7 +13,7 @@ const LoginView = () => {
    const [errorMessage, setErrorMessage] = useState("")
    const initials = { username: "", password: "" };
    const schema = {
-      username: Yup.string().required("Veuilllez saisir votre pseudo"),
+      username: Yup.string().required("Veuillez saisir votre pseudo"),
       password: Yup.string().required("Veuillez saisir un mot de passe"),
    };
    const from = location.state?.from || "/";
@@ -24,44 +25,50 @@ const LoginView = () => {
    }, [])
 
    return (
-      <div className="container">
-        {errorMessage && <p>{errorMessage}</p>}
-         <Formik
-            initialValues={initials}
-            validationSchema={Yup.object(schema)}
-            onSubmit={(values) => {
-                const execAsync = async () => {
-                    try {
-                        const response = await login(values)
-                        // enregistrement de l'utilisateur et de son token en stockage local
-                        setLoggedUser(response.data);
-                        // redirection page d'accueil
-                        navigate(from, { replace: true });
-                    } catch (error) {
-                        const errorData = error?.response?.data
-                        if (errorData) setErrorMessage("Pseudo ou mot de passe incorrect")
+      <main className="container d-flex align-items-center pt-0">
+        <div className="login-card py-5 mx-auto px-3">
+            <h1 className="mt-0 mb-5">TSUMEGO</h1>
+            {errorMessage && <p className="alert alert-danger">{errorMessage}</p>}
+            <Formik
+                initialValues={initials}
+                validationSchema={Yup.object(schema)}
+                onSubmit={(values) => {
+                    const execAsync = async () => {
+                        try {
+                            const response = await login(values)
+                            // enregistrement de l'utilisateur et de son token en stockage local
+                            setLoggedUser(response.data);
+                            // redirection page d'accueil
+                            navigate(from, { replace: true });
+                        } catch (error) {
+                            const errorData = error?.response?.data
+                            if (errorData) setErrorMessage("Pseudo ou mot de passe incorrect")
+                        }
                     }
-                }
-                execAsync()
-            }}
-         >
-            <Form>
-               <div>
-                  <label htmlFor="username">Pseudo</label>
-                  <Field name="username" type="text" />
-                  <ErrorMessage name="username" />
-               </div>
-               <div>
-                  <label htmlFor="password">Mot de passe</label>
-                  <Field name="password" type="password" />
-                  <ErrorMessage name="password" />
-               </div>
-               <div>
-                  <button type="submit">Se connecter</button>
-               </div>
-            </Form>
-         </Formik>
-      </div>
+                    execAsync()
+                }}
+            >
+                <Form>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Pseudo</label>
+                    <Field name="username" type="text" className="form-control"/>
+                    <ErrorMessage name="username" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Mot de passe</label>
+                    <Field name="password" type="password" className="form-control" />
+                    <ErrorMessage name="password" />
+                </div>
+                <div className="mb-4 text-center mt-4">
+                    <button type="submit" className="btn btn-primary">Se connecter</button>
+                </div>
+                <div className="text-center">
+                    <small>Vous n'avez pas de compte? <Link to={"/signup"} className="text-primary">Créez-en un</Link></small>
+                </div>
+                </Form>
+            </Formik>
+        </div>
+      </main>
    );
 };
 
