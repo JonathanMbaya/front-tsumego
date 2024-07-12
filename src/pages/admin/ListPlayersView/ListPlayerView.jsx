d92b3c10cb098a499caed465ae682cc4b6b2d793
IFDEVM <96420756+IFDevM@users.noreply.github.com>
2024-07-12 02:23:58 +0200

    commentaires des pages

--- src/pages/admin/ListPlayersView/ListPlayerView.jsx
+++ src/pages/admin/ListPlayersView/ListPlayerView.jsx
@@ -1,92 +1,98 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Pagination from '../../../components/Pagination/Pagination';
+import axios from 'axios'; // Import de la bibliothèque Axios pour les requêtes HTTP
+import React, { useEffect, useState } from 'react'; // Import des hooks useEffect et useState depuis React
+import Pagination from '../../../components/Pagination/Pagination'; // Import du composant Pagination depuis un chemin spécifique
 
 function ListPlayerView() {
-    const [players, setPlayers] = useState([]);
+    const [players, setPlayers] = useState([]); // État pour stocker la liste des joueurs
 
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-    const [totalCount, setTotalCount] = useState(0);
+    const [page, setPage] = useState(1); // État pour gérer la page actuelle des joueurs
+    const [totalPages, setTotalPages] = useState(1); // État pour stocker le nombre total de pages
+    const [totalCount, setTotalCount] = useState(0); // État pour stocker le nombre total de joueurs
 
     useEffect(() => {
+        // Fonction pour récupérer la liste des joueurs depuis l'API
         const fetchPlayers = async () => {
-        try {
-            const response = await axios.get('http://127.0.0.1:8000/api/users/');
-            if (Array.isArray(response.data.results)) {
-            setPlayers(response.data.results);  // Assurez-vous que response.data contient la liste des joueurs
-            setTotalPages(Math.ceil(response.data.count/response.data.results.length));
-            setTotalCount(response.data.count);
-            } else {
-            console.error('Unexpected response data:', response.data);
+            try {
+                // Appel à l'API pour obtenir les données des joueurs
+                const response = await axios.get('http://127.0.0.1:8000/api/users/');
+
+                // Vérification de la structure de la réponse
+                if (Array.isArray(response.data.results)) {
+                    setPlayers(response.data.results); // Mise à jour de la liste des joueurs
+                    setTotalPages(Math.ceil(response.data.count / response.data.results.length)); // Calcul du nombre total de pages
+                    setTotalCount(response.data.count); // Mise à jour du nombre total de joueurs
+                } else {
+                    console.error('Structure de réponse inattendue :', response.data);
+                }
+            } catch (error) {
+                console.error('Erreur lors de la récupération de la liste des joueurs :', error);
             }
-        } catch (error) {
-            console.error('There was an error fetching the player list!', error);
-        }
         };
 
-        fetchPlayers();
-    }, []);
+        fetchPlayers(); // Appel initial pour charger les joueurs lors du montage du composant
+    }, []); // Dépendance vide pour que useEffect s'exécute une seule fois lors du montage du composant
 
+    // Fonction pour gérer le changement de page des joueurs
     const handlePageChange = (newPage) => {
-        setPage(newPage);
+        setPage(newPage); // Mettre à jour l'état 'page' avec la nouvelle page sélectionnée
     };
 
-  return (
-    <>
-        <h1>Liste de joueurs</h1>
-        <table>
-            <thead>
-            <tr>
-                <th>Pseudo</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Modifier</th>
-                <th>Supprimer</th>
-            </tr>
-            </thead>
-            <tbody>
-            {Array.isArray(players) ? (
-                players.map((player) => (
-                    <tr key={player.id}>
-                        <td>{player.username}</td>
-                        <td>{player.email}</td>
-                        <td>
-                            <select name="statut" id="statut" defaultValue={player.role_id}>
-                                <option value="false">Joueur</option>
-                                <option value="true">Editeur</option>
-                                <option value="true">Administrateur</option>
-                            </select>
-                        </td>
-                        <td>
-                            <span className="material-symbols-outlined">
-                            edit
-                            </span>
-                        </td>
+    return (
+        <>
+            <h1>Liste de joueurs</h1>
 
-                        <td>
-                            <span className="material-symbols-outlined">
-                            delete
-                            </span>
-                        </td>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Pseudo</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Modifier</th>
+                        <th>Supprimer</th>
                     </tr>
-                ))
-            ) : (
-                <tr>
-                <td colSpan="4">No players available</td>
-                </tr>
-            )}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {Array.isArray(players) && players.length > 0 ? (
+                        players.map((player) => (
+                            <tr key={player.id}>
+                                <td>{player.username}</td>
+                                <td>{player.email}</td>
+                                <td>
+                                    <select name="role" id={`role_${player.id}`} defaultValue={player.role_id}>
+                                        <option value="false">Joueur</option>
+                                        <option value="true">Editeur</option>
+                                        <option value="true">Administrateur</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <span className="material-symbols-outlined">
+                                        edit
+                                    </span>
+                                </td>
+                                <td>
+                                    <span className="material-symbols-outlined">
+                                        delete
+                                    </span>
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan="5">Aucun joueur disponible</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
 
-        <Pagination
-            currentPage={page}
-            totalCount={totalCount}
-            pageSize={10}
-            onPageChange={handlePageChange}
-        />
-    </>
-  );
+            {/* Composant Pagination pour la navigation entre les pages */}
+            <Pagination
+                currentPage={page}
+                totalCount={totalCount}
+                pageSize={10} // Taille de la page, supposée être 10 éléments par page
+                onPageChange={handlePageChange}
+            />
+        </>
+    );
 }
 
 export default ListPlayerView;
--- src/routes/PublicRouter.jsx
+++ src/routes/PublicRouter.jsx
@@ -1,64 +1,65 @@
 import React from "react";
-import {createBrowserRouter } from "react-router-dom";
-import ListTsumego from "../pages/ListTsumego";
-import TsumegoGame from "../pages/TsumegoGame";
-import SignupView from "../pages/client/SignupView/SignupView";
-import LoginView from "../pages/client/LoginView/LoginView";
-import ClientLayout from "../pages/client/ClientLayout";
-import ProposeTusmegoView from "../pages/client/ProposeTsumegoView/ProposeTusmegoView";
-import AdminLayout from "../pages/admin/AdminLayout";
-import ListPlayerView from "../pages/admin/ListPlayersView/ListPlayerView";
-import StatsView from "../pages/admin/StatsView/StatsView";
-import ProblemView from "../pages/admin/SubmitProblemsView/ProblemView";
+import { createBrowserRouter } from "react-router-dom"; // Import de la fonction createBrowserRouter depuis react-router-dom
+import ListTsumego from "../pages/ListTsumego"; // Import du composant ListTsumego depuis le chemin spécifié
+import TsumegoGame from "../pages/TsumegoGame"; // Import du composant TsumegoGame depuis le chemin spécifié
+import SignupView from "../pages/client/SignupView/SignupView"; // Import du composant SignupView depuis le chemin spécifié
+import LoginView from "../pages/client/LoginView/LoginView"; // Import du composant LoginView depuis le chemin spécifié
+import ClientLayout from "../pages/client/ClientLayout"; // Import du composant ClientLayout depuis le chemin spécifié
+import ProposeTusmegoView from "../pages/client/ProposeTsumegoView/ProposeTusmegoView"; // Import du composant ProposeTusmegoView depuis le chemin spécifié
+import AdminLayout from "../pages/admin/AdminLayout"; // Import du composant AdminLayout depuis le chemin spécifié
+import ListPlayerView from "../pages/admin/ListPlayersView/ListPlayerView"; // Import du composant ListPlayerView depuis le chemin spécifié
+import StatsView from "../pages/admin/StatsView/StatsView"; // Import du composant StatsView depuis le chemin spécifié
+import ProblemView from "../pages/admin/SubmitProblemsView/ProblemView"; // Import du composant ProblemView depuis le chemin spécifié
 
 const routes = createBrowserRouter([
     {
-        path: "/signup",
-        element: <SignupView />
+        path: "/signup", // Chemin pour l'inscription
+        element: <SignupView /> // Composant à rendre quand le chemin '/signup' est atteint
     },
     {
-        path: "/login",
-        element: <LoginView />
+        path: "/login", // Chemin pour la connexion
+        element: <LoginView /> // Composant à rendre quand le chemin '/login' est atteint
     },
     {
-        path: "/",
-        element: <ClientLayout />,
-        children: [
+        path: "/", // Chemin racine
+        element: <ClientLayout />, // Composant de mise en page pour les pages client
+        children: [ // Routes enfants sous '/client'
             {
-                path: "",
-                element: <ListTsumego />
+                path: "", // Chemin vide correspondant à '/'
+                element: <ListTsumego /> // Composant ListTsumego rendu quand '/client' est atteint
             },
             {
-                path: "listgames",
-                element: <ListTsumego />
+                path: "listgames", // Chemin pour la liste des jeux
+                element: <ListTsumego /> // Composant ListTsumego rendu quand '/client/listgames' est atteint
             },
             {
-                path: "listgames/game/:id",
-                element: <TsumegoGame />
+                path: "listgames/game/:id", // Chemin pour un jeu spécifique
+                element: <TsumegoGame /> // Composant TsumegoGame rendu quand '/client/listgames/game/:id' est atteint
             },
             {
-                path: "submit-problem",
-                element: <ProposeTusmegoView />
+                path: "submit-problem", // Chemin pour soumettre un problème
+                element: <ProposeTusmegoView /> // Composant ProposeTusmegoView rendu quand '/client/submit-problem' est atteint
             }
         ]
     },
     {
-        path: "/admin",
-        element: <AdminLayout />,
-        children: [
+        path: "/admin", // Chemin pour l'administration
+        element: <AdminLayout />, // Composant de mise en page pour les pages d'administration
+        children: [ // Routes enfants sous '/admin'
             {
-                path: "listplayers",
-                element: <ListPlayerView/>
+                path: "listplayers", // Chemin pour la liste des joueurs
+                element: <ListPlayerView/> // Composant ListPlayerView rendu quand '/admin/listplayers' est atteint
             },
             {
-                path: "stats",
-                element: <StatsView/>
+                path: "stats", // Chemin pour les statistiques
+                element: <StatsView/> // Composant StatsView rendu quand '/admin/stats' est atteint
             },
             {
-                path: "problems",
-                element: <ProblemView/>
+                path: "problems", // Chemin pour les problèmes
+                element: <ProblemView/> // Composant ProblemView rendu quand '/admin/problems' est atteint
             }
         ]
     }
-])
-export default routes;
+]);
+
+export default routes; // Exporte les routes définies pour les utiliser dans l'application
